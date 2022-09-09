68e3c77de648106ee6ecf0273cbcb681912d6108
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-09-09 17:24:29 +0100

    Mongo lab User Favourites

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -6,9 +6,17 @@ const Schema = mongoose.Schema;
 // by Mongoose for creating schema instances 
 //(in this case, UserSchema)
 
+const MovieSchema = new Schema({
+  id: Number,
+  title: String
+});
+
 const UserSchema = new Schema({
   username: { type: String, unique: true, required: true},
-  password: {type: String, required: true }
+  password: {type: String, required: true },
+  // an array of favourites that reference Movies
+  // --- POST Favorites ---
+  favourites: [MovieSchema]
 });
 
 export default mongoose.model('User', UserSchema);
