fb38c4f8238192b18a901dc56b8b5cd8d1a401ac
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-09-09 16:47:31 +0100

    Exercise_Lab-API-2: added routing for genres

--- movies-api/index.js
+++ movies-api/index.js
@@ -1,23 +1,24 @@
-import dotenv from 'dotenv';
-import express from 'express';
+import dotenv from 'dotenv'; //+
+import express from 'express'; //+
 import moviesRouter from './api/movies';
+import genresRouter from './api/genres';
 import './db';
 import './seedData';
 import usersRouter from './api/users';
 
-dotenv.config();
+dotenv.config(); //+
 
-const app = express();
+const app = express(); //+
 
-const port = process.env.PORT;
+const port = process.env.PORT; //+
 
 app.use(express.json());
 app.use('/api/movies', moviesRouter);
-// app.use('/api/genres', genresRouter);
+app.use('/api/genres', genresRouter);
 app.use('/api/users', usersRouter);
 
 
-app.listen(port, () => {
+app.listen(port, () => { //+
   console.info(`Server running at ${port}`);
 });
 
