81e6cefa46af021b65bb2a7c1751e138d48fffad
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-09-09 21:05:31 +0100

    Topic 10 - Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt-nodejs';
 
 const Schema = mongoose.Schema;
 
@@ -27,12 +28,44 @@ UserSchema.statics.findByUserName = function (username) {
   return this.findOne({ username: username });
 };
 
-UserSchema.methods.comparePassword = function (candidatePassword) {
-  const isMatch = this.password === candidatePassword;
-  if (!isMatch) {
-    throw new Error('Password mismatch');
-  }
-  return this;
+// UserSchema.methods.comparePassword = function (candidatePassword) {
+//   const isMatch = this.password === candidatePassword;
+//   if (!isMatch) {
+//     throw new Error('Password mismatch');
+//   }
+//   return this;
+// };
+
+// uses bcrypt to compare candidate password to the password 
+// stored in the database
+UserSchema.methods.comparePassword = function (passw, callback) {
+  bcrypt.compare(passw, this.password, (err, isMatch) => {
+    if (err) {
+      return callback(err);
+    }
+    callback(null, isMatch);
+  });
 };
+
+// to include a pre-save hook
+UserSchema.pre('save', function(next) {
+  const user = this;
+  if (this.isModified('password') || this.isNew) {
+      bcrypt.genSalt(10, (err, salt)=> {
+          if (err) {
+              return next(err);
+          }
+          bcrypt.hash(user.password, salt, null, (err, hash)=> {
+              if (err) {
+                  return next(err);
+              }
+              user.password = hash;
+              next();
+          });
+      });
+  } else {
+      return next();
+  }
+});
 
 export default mongoose.model('User', UserSchema);
