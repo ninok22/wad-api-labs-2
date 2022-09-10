efef4170d66224d2316f8d402ba0700d45ebe8c5
Nino Kelehsashvili <20074819@mail.wit.ie>
2022-09-10 17:57:25 +0100

    Topic 11 - React App

---
+++ reactApp/src/api/movie-api.js
@@ -0,0 +1,24 @@
+export const login = (username, password) => {
+    return fetch('/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+export const signup = (username, password) => {
+    return fetch('/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    }).then(res => res.json())
+};
+
+/* Notice how the routes match what is implemented in the Express API 
+    and what you tested using postman. The proxy property in the React 
+    package.json provides the host information required to complete the 
+    request(i.e. localhost:8080). */
